8e3de2786c5a4c9524d61c60a8a0a4edc6b0a3cf
Gavin-NB <91671075+Gavin-NB@users.noreply.github.com>
2022-02-10 17:48:54 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,8 @@
 sudo apt update
 sudo apt install postgresql postgresql-contrib
-sudo -i -u postgres #to enter the postgres session
 sudo -u postgres createuser --interactive
 #insert name of user which is mob_db_user
-ALTER USER mob_db_user PASSWORD 'mob_db_pass';
-#to exit the interactive postgres session, user must type '\q'
+sudo -u postgres psql -c "ALTER ROLE mob_db_user WITH password 'mob_db_pass'"
 sudo -u postgres createuser --interactive
 #insert name of user which is mob_db_user
 ALTER USER mob_app_usr PASSWORD 'mob_app_pass';
