5b4d28b4ae480f0570adee49ebc257d2684d9c5a
Patricia Osifo-Iduorobo <92851998+PatProMath@users.noreply.github.com>
2022-02-10 05:04:53 +0100

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#To install PostgreSQL on Ubuntu 20.04
+# Run Ubuntu 20.04 system update 
+sudo apt update
+#Add PostgreSQL GPG key & repository
+wget -q https://www.postgresql.org/media/keys/ACCC4CF8.asc -O- | sudo apt-key add -
+#Add repository in Ubuntu 20.04
+echo "deb [arch=amd64] http://apt.postgresql.org/pub/repos/apt/ focal-pgdg main" | sudo tee /etc/apt/sources.list.d/postgresql.list
+# Run system update command
+sudo apt update
+sudo apt install postgresql-13
+sudo systemctl status postgresql
+# Add user
+sudo useradd mob_db_user
+sudo passwd mob_db_user
