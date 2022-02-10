36c61065e2c1f96f1b5caa0e58adfa15663937d7
Gavin-NB <91671075+Gavin-NB@users.noreply.github.com>
2022-02-10 18:13:39 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ sudo -u postgres createuser --interactive
 #insert name of user which is mob_db_user
 sudo -u postgres psql -c "ALTER ROLE mob_db_user WITH password 'mob_db_pass'"
 sudo -u postgres createuser --interactive
-#insert name of user which is mob_db_user
-ALTER USER mob_app_usr PASSWORD 'mob_app_pass';
-#to exit the interactive postgres session, user must type '\q'
+#insert name of user which is mob_app_usr
+sudo -u postgres psql -c "ALTER ROLE mob_db_user WITH password 'mob_app_pass'"
+#sudo -i -u postgress to enter into the postgres session
+#to exit the interactive postgres session, user must type 'exit'
