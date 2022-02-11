560b0a70f74aaa72f8743f066805fd0223e968d5
Oluwaseyi Daniel <65072332+redmicelles@users.noreply.github.com>
2022-02-11 03:21:40 +0100

    Add files via upload
    setup.sh updated

--- setup.sh
+++ setup.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+sudo dpkg -s posgresql
+
+if [ $? -eq 0 ]
+then
+  sudo apt-get --purge remove postgresql
+  sudo rm -rf /var/lib/postgresql/
+  sudo rm -rf /var/log/postgresql/
+  sudo rm -rf /etc/postgresql/
+fi
+
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
 
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
@@ -8,3 +18,14 @@ sudo apt-get update
 sudo apt-get -y install postgresql
 
 sudo -u postgres psql -c "create user mod_db_user  with password 'mod_db_pass';"
+
+sudo systemctl is-active --quiet  postgresql
+
+if [ $? -ne 0 ]
+then
+  sudo systemctl start --quiet postgresql
+  sudo systemctl enable --quiet postgresql
+fi
+
+echo "Postgresql installation completed"
+
