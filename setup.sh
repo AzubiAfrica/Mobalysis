ab01c736fc4135f77e1451f39db881480a87deb3
kolade <koladeayeni@gmail.com>
2022-02-11 14:08:27 +0100

    This Step to  clones the repository into the home directory of the mob_app_usr.

---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#Step 1
+
+
+
+
+
+
+
+
+
+#Step 2
+
+
+
+
+
+
+#Step 3
+#This script clones the repository into the home directory of the mob_app_usr
+cd /home/mob_app_usr
+git clone https://github.com/koladeA/Mobalysis
