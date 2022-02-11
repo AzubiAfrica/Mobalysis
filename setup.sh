6819770315b1da091046d5554b9731ecf90f1af2
Dennis Awer Mensah <ornisah@gmail.com>
2022-02-11 16:44:34 +0000

    Created  new user and user's home directory

--- setup.sh
+++ setup.sh
@@ -11,7 +11,11 @@
 
 
 #Step 2
-
+# -d /home/mob_app_usr/ – Set /home/mob_app_usr/ as home directory of the new user
+# -m – Create the user’s home directory
+# -G sudo – Make sure mob_app_usr user can sudo i.e. give admin access to the new user
+sudo useradd -s /bin/bash -d /home/mob_app_usr/ -m -G sudo mob_app_usr
+sudo passwd mob_app_usr
 
 
 
