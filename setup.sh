c063b69ea0eb90a9219e77001bf48b7a89b9e367
jwangui <wanguijane468@gmail.com>
2022-02-11 16:26:10 +0300

    task 2 Add User

--- setup.sh
+++ setup.sh
@@ -6,3 +6,9 @@ sudo apt-get update -y
 sudo apt-get install postgresql postgresql-contrib -y
 sudo -u postgres createuser --interactive 
 sudo -u postgres -c "ALTER ROLE mob_db_user WITH ENCRYPTED PASSWORD 'mob_db_pass'" 
+
+#Script to create a mob_app_usr and install a home directory for the user
+
+#add user with a home directory
+sudo useradd mod_app_usr -m
+cd /etc/home/mod_app_usr
