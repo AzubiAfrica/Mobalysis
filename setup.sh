1f2c315eab3362806fc5c21fbaf7804bbe7d0275
Oluwaseyi Daniel <65072332+redmicelles@users.noreply.github.com>
2022-02-11 04:07:40 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
-sudo dpkg -s posgresql
-
-if [ $? -eq 0 ]
-then
-  sudo apt-get --purge remove postgresql
-  sudo rm -rf /var/lib/postgresql/
-  sudo rm -rf /var/log/postgresql/
-  sudo rm -rf /etc/postgresql/
-fi
-
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
 
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
@@ -16,8 +6,6 @@ wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-
 sudo apt-get update
 
 sudo apt-get -y install postgresql
-
-sudo -u postgres psql -c "create user mod_db_user  with password 'mod_db_pass';"
 
 sudo systemctl is-active --quiet  postgresql
 
@@ -27,5 +15,6 @@ then
   sudo systemctl enable --quiet postgresql
 fi
 
-echo "Postgresql installation completed"
+sudo -u postgres psql -c "create user mod_db_user  with password 'mod_db_pass';"
 
+echo "Postgresql installation completed"
