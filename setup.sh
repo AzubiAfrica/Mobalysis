9af81a0d28a85a7063feb05cb91b6ece9bcec5ac
Dennis Awer Mensah <ornisah@gmail.com>
2022-02-14 17:14:09 +0000

    Removed password od mob_app_usr

--- setup.sh
+++ setup.sh
@@ -15,7 +15,7 @@
 # -m – Create the user’s home directory
 # -G sudo – Make sure mob_app_usr user can sudo i.e. give admin access to the new user
 sudo useradd -s /bin/bash -d /home/mob_app_usr/ -m -G sudo mob_app_usr
-sudo passwd mob_app_usr
+
 
 
 
