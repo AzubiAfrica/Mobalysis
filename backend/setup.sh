691a68494d9759be3ed5b1bfa47db4a97f3a7a53
bongani mokase <bongani.mokase@azubiafrica.org>
2022-02-14 19:53:00 +0200

    Add OS user mob_app_usr and install a home directory for the user

--- backend/setup.sh
+++ backend/setup.sh
@@ -1,1 +1,11 @@
-#jufjgyfjgyhfg
+#Modify the setup.sh script to add an operating system user mob_app_usr 
+#The script should install a home directory for the user
+
+username="mob_app_usr"
+if getent passwd "$username" > /dev/null 2>&1; then
+    #echo "The username '$username' exits."
+else
+    #echo "The user '$username' does not exit. Lets create the user.
+    useradd mob_app_usr -m -d /home/mob_app_usr
+    #echo "User successfully created"
+fi
