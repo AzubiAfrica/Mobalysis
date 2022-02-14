f999eec62a193e2fca8d5d039af6b5392795ec3d
koladeA <koladeayeni@gmail.com>
2022-02-14 19:45:31 +0100

    Delete install.sh

--- install.sh
+++
@@ -1,5 +0,0 @@
-cd /home/mob_app_usr
-git clone https://github.com/koladeA/Mobalysis
-
-#Modify install.sh shell script.When executed install.sh should: Add the following environment variables and values to the mob_app_usr’s bashrc file
-echo -e "DBNAME =mobalytics\nDBUSER=mob_db_user\nDBPASS=mob_db_pass\nDBHOST=localhost\nDBPORT=5432" >>config.bashrc
