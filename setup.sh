6c2b33acc1dca7d338c8ee497752212ecbd187eb
Gavin-NB <91671075+Gavin-NB@users.noreply.github.com>
2022-02-14 11:13:44 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,8 @@
 sudo apt update
 sudo apt install postgresql postgresql-contrib
-sudo -u postgres createuser --interactive
-#insert name of user which is mob_db_user
+sudo -u postgres createuser 'mob_db_user'
 sudo -u postgres psql -c "ALTER ROLE mob_db_user WITH password 'mob_db_pass'"
-sudo -u postgres createuser --interactive
-#insert name of user which is mob_app_usr
+sudo -u postgres createuser 'mob_app_usr'
 sudo -u postgres psql -c "ALTER ROLE mob_app_usr WITH password 'mob_app_pass'"
 #sudo -i -u postgress to enter into the postgres session
 #to exit the interactive postgres session, user must type 'exit'
-sudo mkdir mob_app_usr
-cd /home/mob_app_usr
-sudo git clone https://github.com/Gavin-NB/Mobalysis.git
