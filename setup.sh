828a3fdbf67506b36360ae57176729e567a98f90
clns53 <mt-i@DESKTOP-DAKR5TD.localdomain>
2022-02-14 22:55:18 +0300

    Modified setup.sh to creat an operating system user mob_app_usr.

--- setup.sh
+++ setup.sh
@@ -6,3 +6,6 @@ sudo -u postgres psql
 sudo service postgresql start
 sudo -u postgres createuser --interactive
 CREATE USER mob_db_user PASSWORD 'mob_db_pass'
+\q
+exit
+sudo useradd mob_app_usr -p 'Project4'
