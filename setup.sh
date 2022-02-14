7a343d6b7c8a31f681b28dd6467abaa3f1d58796
oziuzoma <oziuzoma@gmail.com>
2022-02-14 23:24:47 +0000

    Script to install python3-venv and create and database mobalytics and set owner to mob_db_user

--- setup.sh
+++ setup.sh
@@ -1,14 +1,24 @@
-#!/bin/bash
 
+#!/bin/bash
+  
 #Script to install PostgreSQL and create a user mob_db_user with a password mob_db_pass
 
 sudo apt-get update -y
+sudo apt-get upgrade -y
 sudo apt-get install postgresql postgresql-contrib -y
-sudo -u postgres createuser 'mob_db_user' 
-sudo -u postgres -c "ALTER ROLE mob_db_user WITH ENCRYPTED PASSWORD 'mob_db_pass'" 
+sudo -u postgres createuser 'mob_db_user'
+sudo -u postgres -c "ALTER ROLE mob_db_user WITH ENCRYPTED PASSWORD 'mob_db_pass'"
 
 #Script to create a mob_app_usr and install a home directory for the user
 
 #add user with a home directory
 sudo useradd mod_app_usr -m
-cd /etc/home/mod_app_usr
+cd /home/mod_app_usr
+
+#Script to install python3-venv and create and database mobalytics and set owner to mob_db_user
+
+sudo -u postgres createdb -O mob_db_user mobalytics
+sudo apt-get install python3-pip -y
+sudo apt-get install python3-venv -y
+
+
