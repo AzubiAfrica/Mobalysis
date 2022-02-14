1e688f62b4c82b06a31f98a2beab966ac0d0cd9e
unknown <alaoemmanuel2014@gmail.com>
2022-02-14 19:51:34 +0100

    Setup file to install and setup Postgre

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt update
+sudo apt-get -y install postgresql
+sudo su postgres
+sudo -u postgres psql
+sudo service postgresql start
+sudo -u postgres createuser --interactive
+CREATE USER mob_db_user PASSWORD 'mob_db_pass'
