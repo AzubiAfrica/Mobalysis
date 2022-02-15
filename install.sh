d877bf82e38c3990bbca884148fadaef2781c37e
JenJepkirui <97353477+JenJepkirui@users.noreply.github.com>
2022-02-15 17:29:36 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,13 @@
 sudo mkdir mob_app_usr
 cd /home/mob_app_usr
 sudo git clone https://github.com/Gavin-NB/Mobalysis.git
+cd /home/mob_app_usr
+#!/bin/bash
+DBNAME ="mobalytics"
+DBUSER="mob_db_user"
+DBPASS="mob_db_pass"
+DBHOST="localhost"
+DBPORT=5432
+sudo $printenv
+echo >> ~/. bashrc
+$ source ~/.bashrc
