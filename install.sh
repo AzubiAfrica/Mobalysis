1b855ade4ecc3ec3e4b8a2045157a6315b6bdedb
Leon Mayanja <manzileon@gmail.com>
2022-02-15 19:19:30 +0200

    Task 1, 2 and 3 update

--- install.sh
+++ install.sh
@@ -1,13 +1,20 @@
-sudo mkdir mob_app_usr
+#!/bin/bash
 cd /home/mob_app_usr
 sudo git clone https://github.com/Gavin-NB/Mobalysis.git
-cd /home/mob_app_usr
-#!/bin/bash
-DBNAME ="mobalytics"
-DBUSER="mob_db_user"
-DBPASS="mob_db_pass"
-DBHOST="localhost"
-DBPORT=5432
-sudo $printenv
-echo >> ~/. bashrc
-$ source ~/.bashrc
+#cd /home/mob_app_usr
+#DBNAME ="mobalytics"
+#DBUSER="mob_db_user"
+#DBPASS="mob_db_pass"
+#DBHOST="localhost"
+#DBPORT=5432
+#sudo $printenv
+#echo >> ~/. bashrc
+#$ source ~/.bashrc
+## Create a virtual environment called env
+#python3 -m venv env
+#
+## Activate the virtual environment
+#source env/bin/activate
+#
+## Install the application packages in the virtual environment
+#python3 -m pip install -r requirements.txt
--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
+#!/bin/bash
 sudo apt update
 sudo apt install postgresql postgresql-contrib
 sudo -u postgres createuser 'mob_db_user'
 sudo -u postgres psql -c "ALTER ROLE mob_db_user WITH password 'mob_db_pass'"
-sudo -u postgres createuser 'mob_app_usr'
-sudo -u postgres psql -c "ALTER ROLE mob_app_usr WITH password 'mob_app_pass'"
+sudo useradd -m -d /home/mob_app_usr mob_app_usr
 #sudo -i -u postgress to enter into the postgres session
 #to exit the interactive postgres session, user must type 'exit'
-sudo apt-get install python3-pip
-sudo pip3 install virtualenv 
-virtualenv python3-venv 
-sudo -u postgres createdb mobalytics
-sudo -u postgres psql -c "ALTER DATABASE mobalytics OWNER TO mob_db_user"
+#sudo apt-get install python3-pip
+#sudo pip3 install virtualenv 
+#virtualenv python3-venv 
+#sudo -u postgres createdb mobalytics
+#sudo -u postgres psql -c "ALTER DATABASE mobalytics OWNER TO mob_db_user"
