c8537e6571a8f247627b4f2ec086ddd074b36de9
Gavin-NB <91671075+Gavin-NB@users.noreply.github.com>
2022-02-15 13:25:48 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,3 +6,8 @@ sudo -u postgres createuser 'mob_app_usr'
 sudo -u postgres psql -c "ALTER ROLE mob_app_usr WITH password 'mob_app_pass'"
 #sudo -i -u postgress to enter into the postgres session
 #to exit the interactive postgres session, user must type 'exit'
+sudo apt-get install python3-pip
+sudo pip3 install virtualenv 
+virtualenv python3-venv 
+sudo -u postgres createdb mobalytics
+sudo -u postgres psql -c "ALTER DATABASE mobalytics OWNER TO mob_db_user"
