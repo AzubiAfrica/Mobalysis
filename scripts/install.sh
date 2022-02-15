8b5f399fb230f1ba3dd6e605c98a46b6db3c497b
kolade <koladeayeni@gmail.com>
2022-02-15 17:37:39 +0100

    Update

--- scripts/install.sh
+++ scripts/install.sh
@@ -13,13 +13,6 @@ sudo chmod +x install.sh
 #Step 4
 #This adds the environmental variable to mob_db_user
 
-<<<<<<< HEAD
-echo -e "DBNAME=mobalytics\nDBUSER=mob_db_user\nDBPASS=mob_db_pass\nDBHOST=localhost\nDBPORT=5432" >> .bashrc
-
-sudo chmod +x .bashrc
-
-=======
 echo -e "DBNAME=mobalytics\nDBUSER=mob_db_user\nDBPASS=mob_db_pass\nDBHOST=localhost\nDBPORT=5432" >> .bashrc
 
 chmod +x .bashrc
->>>>>>> 1ed673af90ea1455dc4cdfacccde233dccb11e37
