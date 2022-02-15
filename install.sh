91ee81fd969e581a50625037bc22c56105b0c297
curhvyn <michaelcurhvyn@gmail.com>
2022-02-15 16:56:12 +0000

    updated install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#clone the repository
+git clone https://github.com/President-Banda/Mobalysis.git
+
+#Move project files to mob_app_usr home directory
+sudo mv Mobalysis /home/mob_app_usr/
