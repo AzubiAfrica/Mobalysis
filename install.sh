5db7f381fd5d2057ace85f99b723f25d3b660606
kolade <koladeayeni@gmail.com>
2022-02-15 15:38:56 +0100

    Changes made to install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,15 @@
+#!/bin/bash
+
+#Step 3
+#This script clones the repository into the home directory of the mob_app_usr
+
+
 cd /home/mob_app_usr
 git clone https://github.com/koladeA/Mobalysis
 
-#Modify install.sh shell script.When executed install.sh should: Add the following environment variables and values to the mob_app_usr’s bashrc file
-echo -e "DBNAME =mobalytics\nDBUSER=mob_db_user\nDBPASS=mob_db_pass\nDBHOST=localhost\nDBPORT=5432" >>config.bashrc
+chmod +x install.sh
+
+
+#Step 4
+#This adds the environmental variable to mob_db_user
+echo -e "DBNAME =mobalytics\nDBUSER=mob_db_user\nDBPASS=mob_db_pass\nDBHOST=localhost\nDBPORT=5432" >>.bashrc
