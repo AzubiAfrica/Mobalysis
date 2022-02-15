c3e2921f557ddab2c360df1219736da55e448f21
Oluwaseyi Daniel <dexter_daniel@outlook.com>
2022-02-15 16:45:40 +0100

    tasks 9 and 10 completed

--- install.sh
+++ install.sh
@@ -1,22 +1,26 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #Clone repo and  create environment varables
 sudo -i -u mob_app_usr bash << EOF
 #clone mobalysis repo
-git clone https://github.com/redmicelles/Mobalysis
+# git clone https://github.com/redmicelles/Mobalysis
 
-#set Environment Variables
-echo export DBNAME=\'mobalytics\' >> .profile
-echo export DBUSER=\'mob_db_user\' >> .profile
-echo export DBPASS=\'mob_db_pass\' >> .profile
-echo export DBHOST=\'localhost\' >> .profile
-echo export DBPORT=\'5432\' >> .profile
+# #set Environment Variables
+# echo export DBNAME=\'mobalytics\' >> .profile
+# echo export DBUSER=\'mob_db_user\' >> .profile
+# echo export DBPASS=\'mob_db_pass\' >> .profile
+# echo export DBHOST=\'localhost\' >> .profile
+# echo export DBPORT=\'5432\' >> .profile
+
+#create and activate venv
+python3 -m venv ~/Mobalysis/venv
+source  ~/Mobalysis/venv/bin/activate
+
+#pip install requirements
+pip3 install -r ~/Mobalysis/backend/requirements.txt
+
+#makemigrations
+python3 ~/Mobalysis/backend/manage.py makemigrations
+
+#migrate django models
+python3 ~/Mobalysis/backend/manage.py migrate
 EOF
-
-#install pip 
-# pip -V
-# if [[ $? -gt 0 ]]
-# then 
-# sudo apt install -y python3-pip
-# fi
-# # create the virtual env (env), active it, and install necessary app packages
-# sudo su - mob_app_usr -c "virtualenv env && . env/bin/activate && pip install -r ./Mobalysis/backend/requirements.txt"
