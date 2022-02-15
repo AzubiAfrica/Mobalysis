b0af6525a8ca5b1cdd6cc0635073842abf7c9034
Oluwaseyi Daniel <dexter_daniel@outlook.com>
2022-02-15 16:14:19 +0100

    install modified to .profile instead of .bashrc

--- install.sh
+++ install.sh
@@ -5,19 +5,18 @@ sudo -i -u mob_app_usr bash << EOF
 git clone https://github.com/redmicelles/Mobalysis
 
 #set Environment Variables
-echo export DBNAME=\'mobalytics\' >> .bashrc
-echo export DBUSER=\'mob_db_user\' >> .bashrc
-echo export DBPASS=\'mob_db_pass\' >> .bashrc
-echo export DBHOST=\'localhost\' >> .bashrc
-echo export DBPORT=\'5432\' >> .bashrc
-sudo -s source ~/.bashrc
+echo export DBNAME=\'mobalytics\' >> .profile
+echo export DBUSER=\'mob_db_user\' >> .profile
+echo export DBPASS=\'mob_db_pass\' >> .profile
+echo export DBHOST=\'localhost\' >> .profile
+echo export DBPORT=\'5432\' >> .profile
 EOF
 
 #install pip 
-pip -V
-if [[ $? -gt 0 ]]
-then 
-sudo apt install -y python3-pip
-fi
-# create the virtual env (env), active it, and install necessary app packages
-sudo su - mob_app_usr -c "virtualenv env && . env/bin/activate && pip install -r ./Mobalysis/backend/requirements.txt"
+# pip -V
+# if [[ $? -gt 0 ]]
+# then 
+# sudo apt install -y python3-pip
+# fi
+# # create the virtual env (env), active it, and install necessary app packages
+# sudo su - mob_app_usr -c "virtualenv env && . env/bin/activate && pip install -r ./Mobalysis/backend/requirements.txt"
