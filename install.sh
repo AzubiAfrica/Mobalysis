5813340804a9023541118d4eb8cc62e0630c713c
Cynthia Iradukunda <c.iradukund@alustudent.com>
2022-02-15 15:58:15 +0200

    create the virtualenv and install packages

--- install.sh
+++ install.sh
@@ -2,6 +2,10 @@
 sudo su - mob_app_usr -c"git clone https://github.com/redmicelles/Mobalysis"
 
 #install pip 
+pip -V
+if [[ $? -gt 0 ]]
+then 
 sudo apt install -y python3-pip
+fi
 # create the virtual env (env), active it, and install necessary app packages
-sudo su - mob_app_usr -c "virtualenv env && . env/bin/activate && pip3 install -r ./Mobalysis/backend/requirements.txt"
+sudo su - mob_app_usr -c "virtualenv env && . env/bin/activate && pip install -r ./Mobalysis/backend/requirements.txt"
