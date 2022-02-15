99858c8895b1a623f9c2d04392a13cf8d6c721ee
Oluwaseyi Daniel <65072332+redmicelles@users.noreply.github.com>
2022-02-15 18:15:58 +0100

    Update install.sh
    Uncommented required sections

--- install.sh
+++ install.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 #Clone repo and  create environment varables
 sudo -i -u mob_app_usr bash << EOF
-#clone mobalysis repo
-# git clone https://github.com/redmicelles/Mobalysis
 
-# #set Environment Variables
-# echo export DBNAME=\'mobalytics\' >> .profile
-# echo export DBUSER=\'mob_db_user\' >> .profile
-# echo export DBPASS=\'mob_db_pass\' >> .profile
-# echo export DBHOST=\'localhost\' >> .profile
-# echo export DBPORT=\'5432\' >> .profile
+#clone mobalysis repo
+git clone https://github.com/redmicelles/Mobalysis
+
+#set Environment Variables
+echo export DBNAME=\'mobalytics\' >> .profile
+echo export DBUSER=\'mob_db_user\' >> .profile
+echo export DBPASS=\'mob_db_pass\' >> .profile
+echo export DBHOST=\'localhost\' >> .profile
+echo export DBPORT=\'5432\' >> .profile
 
 #create and activate venv
 python3 -m venv ~/Mobalysis/venv
