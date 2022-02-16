1d715defaf28c70d1036ce83b107787a914461ea
jwangui <wanguijane468@gmail.com>
2022-02-16 12:40:40 +0300

    update install dot sh

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# To clone into current directory
+sudo su -c 'git clone https://github.com/wanguij/Mobalysis.git' - mob_app_usr
+
 #Script to create a virtual enviroment env, activate virtual enviroment 
 #source/path and install the application packages in the virtusl enviroment
 
@@ -8,4 +11,16 @@ sudo pip3 install virtualenv
 virtualenv venv 
 
 #command to activate virtual envireoment
-source <venv>/bin/activate
+source venv/bin/activate
+
+#to confirm the virtualenv path
+which python
+
+#command to install packages in the virtualenv
+python3 -m pip install requests
+
+#leave virtual env
+deativate
+
+
+
