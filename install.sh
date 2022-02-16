a57e3f01af62603608413c3925292f21ccda2e88
Rove-Sudo <veronmbuthia@gmail.com>
2022-02-16 21:09:50 +0300

    Install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#Step 3 creating install.sh (clones the repository into the home directory of the mob_app_usr) VERONICA
+
+cd /home/mob_app_usr
+git clone https://github.com/sdg000/Mobalysis
+
+
+sudo chmod +x install.sh
+
+
