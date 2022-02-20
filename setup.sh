a723aeb62485c2a09ad1a30c1bbbab7abfabb6a4
unknown <alaoemmanuel2014@gmail.com>
2022-02-20 19:01:26 +0100

    Edit after corrections from Joseph

--- setup.sh
+++ setup.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 sudo apt update
+sudo apt upgrade -y
 sudo apt-get -y install postgresql
-sudo su postgres
-sudo -u postgres psql
-sudo service postgresql start
-sudo -u postgres createuser --interactive
-CREATE USER mob_db_user PASSWORD 'mob_db_pass'
+sudo -u postgresql psql -c "CREATE USER mob_db_user PASSWORD 'mob_db_pass'"
 \q
 exit
 sudo useradd mob_app_usr -p 'Project4'
