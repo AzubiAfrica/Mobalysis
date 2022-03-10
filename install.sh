2ea7bde3615858a4e1b9c8faefba594f6e5273d1
oziuzoma <99350367+oziuzoma@users.noreply.github.com>
2022-03-10 13:04:39 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,11 +5,11 @@ cd /home/mob_app_usr
 sudo git clone https://github.com/wanguij/Mobalysis.git 
 
 # Add environment variables and values to mob_app_usr's bashrc file
-sudo  bash -c 'echo "export DBUSER=mob_db_user" >> /home/mob_app_user/.bashrc'
-sudo  bash -c 'echo "export DBNAME=mobalysis" >> /home/mob_app_user/.bashrc'
-sudo  bash -c 'echo "export DBHOST=127.0.0.1" >> /home/mob_app_user/.bashrc'
-sudo  bash -c 'echo "export DBPASS=mobdbpass" >> /home/mob_app_user/.bashrc'
-sudo  bash -c 'echo "export DBPORT=5432" >> /home/mob_app_user/.bashrc'
+sudo  bash -c 'echo "export DBUSER=mob_db_user" >> /home/mob_app_usr/.bashrc'
+sudo  bash -c 'echo "export DBNAME=mobalysis" >> /home/mob_app_usr/.bashrc'
+sudo  bash -c 'echo "export DBHOST=127.0.0.1" >> /home/mob_app_usr/.bashrc'
+sudo  bash -c 'echo "export DBPASS=mobdbpass" >> /home/mob_app_usr/.bashrc'
+sudo  bash -c 'echo "export DBPORT=5432" >> /home/mob_app_usr/.bashrc'
 sudo -u mob_app_user sh -c 'cd /home/mob_app_user/ && /usr/bin/python3 -m venv .env'
 sudo -u mob_app_user bash -c 'cd /home/mob_app_user/ && source /home/mob_app_user/.env/bin/activate && pip3 install -r /home/mob_app_user/Mobalysis/backend/requirements.txt && python /home/mob_app_user/Mobalysis/backend/manage.py migrate'
 
@@ -37,5 +37,5 @@ python3 -m pip install requests
 deactivate
 
 #Command to install the application database
-python3 /home/mob_app_usr/mobalysis/backend/backend/manage.py makemigrations
-python3 /home/mob_app_usr/mobalysis/backend/backend/manage.py migrate
+python3 /home/mob_app_usr/Mobalysis/backend/backend/manage.py makemigrations
+python3 /home/mob_app_usr/Mobalysis/backend/backend/manage.py migrate
