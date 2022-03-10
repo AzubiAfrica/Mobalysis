82b9e8607512b7b15acc4c797b52f82a4c76639b
Stanley_Kangethe <94608769+StanleyKangethe@users.noreply.github.com>
2022-03-10 14:47:51 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,13 +5,14 @@ cd /home/mob_app_usr
 sudo git clone https://github.com/wanguij/Mobalysis.git 
 
 # Add environment variables and values to mob_app_usr's bashrc file
-cd /home/mob_app_usr/.bashrc
-export DBNAME=mobalytics
-export DBUSER=mob_db_user
-export DBPASS=mob_db_pass
-export DBHOST=localhost
-export DBPORT=5432
-echo "$DBNAME,$DBUSER,$DBPASS,$DBHOST,$DBPORT"
+sudo  bash -c 'echo "export DBUSER=mob_db_user" >> /home/mob_app_user/.bashrc'
+sudo  bash -c 'echo "export DBNAME=mobalysis" >> /home/mob_app_user/.bashrc'
+sudo  bash -c 'echo "export DBHOST=127.0.0.1" >> /home/mob_app_user/.bashrc'
+sudo  bash -c 'echo "export DBPASS=mobdbpass" >> /home/mob_app_user/.bashrc'
+sudo  bash -c 'echo "export DBPORT=5432" >> /home/mob_app_user/.bashrc'
+sudo -u mob_app_user sh -c 'cd /home/mob_app_user/ && /usr/bin/python3 -m venv .env'
+sudo -u mob_app_user bash -c 'cd /home/mob_app_user/ && source /home/mob_app_user/.env/bin/activate && pip3 install -r /home/mob_app_user/Mobalysis/backend/requirements.txt && python /home/mob_app_user/Mobalysis/backend/manage.py migrate'
+
 
 #Execute changes to the current session of the bashrc file
 source .bashrc
