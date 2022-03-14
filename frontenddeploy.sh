6968163d86996cc4b567ffcd0caf75c8f9b13085
jwangui <wanguijane468@gmail.com>
2022-03-14 14:31:33 +0300

    install nginx and curl code

--- frontenddeploy.sh
+++ frontenddeploy.sh
@@ -2,6 +2,19 @@
 
 
 
+
+#install nginx 
+sudo apt -get install nginx
+
+#Check the nginx version and validate its installation
+nginx -v 
+
+sudo apt -get install curl
+curl --version
+
+#install nodejs 
+sudo apt -get install -y nodejs 
+
 #Deploy the react app inside /var/www/html directory and run it
 sudo cd /var/www/html
 sudo git clone https://github.com/wanguij/Mobalysis
