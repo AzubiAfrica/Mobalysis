e7929d59d41d5a8aafeaafb1b178abd2be12e4ca
Dennis Awer Mensah <ornisah@gmail.com>
2022-03-14 11:52:14 +0000

    Deployed react

--- frontend/scripts/deploy.sh
+++ frontend/scripts/deploy.sh
@@ -4,4 +4,12 @@ sudo apt-get update
 #Upgrade ubuntu
 sudo apt-get upgrade -y
 
-# Update the code
+#Create the react-app
+npx create-react-app react-tutorial
+
+#Build the react project
+cd react-tutorial
+npm run build
+
+#Run the project
+npm start
