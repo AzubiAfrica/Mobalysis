851974775ae70ddf2f3968955a8a46f80714bcb9
Oluwaseyi Daniel <dexter_daniel@outlook.com>
2022-03-15 05:48:18 +0100

    tasks 14 and 15 accomplished

--- install_frontend.sh
+++ install_frontend.sh
@@ -1,6 +1,6 @@
 # Update packages 
-sudo apt-get update -y 
-sudo apt-get upgrade -y
+# sudo apt-get update -y 
+# sudo apt-get upgrade -y
 
 #Add NodeJs PPA to the system 
 curl -sL https://deb.nodesource.com/setup_16.x -o nodesource_setup.sh
@@ -17,5 +17,25 @@ then
   echo "NodeJS successfully created"
 else
   echo "NodeJs installation failed"
-  exit 1
 fi
+
+#install Nginx
+sudo apt -y install nginx
+if [ $? eq  0]
+then
+  echo "Nginx installed sucessfully"
+else
+  echo "Nginx installation failed!"
+fi
+
+#Allow Nginx through firewall
+sudo ufw allow 'Nginx HTTP'
+
+#Enable and restart Nginx service
+sudo systemctl enable nginx
+sudo systemctl restart nginx
+
+#CD to WWW directory and clone the frontend directory from the repo
+sudo git clone --branch dev https://github.com/redmicelles/Mobalysis /var/www/html/tmp
+sudo mv /var/www/html/tmp/frontend /var/www/html
+sudo rm -r /var/www/html/tmp
