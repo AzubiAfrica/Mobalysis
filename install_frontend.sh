3c4da156f8df4102e2d5d16f24ad34dcb9073795
Cynthia Iradukunda <c.iradukund@alustudent.com>
2022-03-17 22:53:21 +0200

    added the frontend playbook tasks

--- install_frontend.sh
+++ install_frontend.sh
@@ -36,3 +36,19 @@ sudo systemctl restart nginx
 sudo git clone --branch dev https://github.com/redmicelles/Mobalysis /var/www/html/tmp
 sudo mv /var/www/html/tmp/frontend /var/www/html
 sudo rm -r /var/www/html/tmp
+
+#remove the nginx files 
+cd /var/www/html 
+rm index.nginx-debian.html index.html
+
+#install the necessary packages and build
+cd /var/www/html/frontend
+npm install 
+npm build
+
+#move the build files to /var/www/html
+sudo mv /var/www/html/frontend/build/* /var/www/html 
+
+#delete the frontend
+sudo rm -r /var/www/html/frontend
+
