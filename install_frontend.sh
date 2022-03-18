a31b532def1c2a246ce43a71117b72bd01824b7b
Cynthia Iradukunda <c.iradukund@alustudent.com>
2022-03-18 09:25:21 +0200

    added script to run the repo

--- install_frontend.sh
+++ install_frontend.sh
@@ -36,22 +36,18 @@ sudo systemctl restart nginx
 sudo git clone --branch dev https://github.com/redmicelles/Mobalysis /var/www/html/tmp
 sudo mv /var/www/html/tmp/frontend /var/www/html
 sudo rm -r /var/www/html/tmp
+
+#remove the nginx files 
+rm /var/www/html/* 
+
 #Install Dependenies
 sudo cd /var/www/html/frontend
 sudo npm install
+sudo npm build
 
-# #remove the nginx files 
-# cd /var/www/html 
-# rm index.nginx-debian.html index.html
+#move the build files to /var/www/html
+sudo mv /var/www/html/frontend/build/* /var/www/html 
 
-# #install the necessary packages and build
-# cd /var/www/html/frontend
-# npm install 
-# npm build
-
-# #move the build files to /var/www/html
-# sudo mv /var/www/html/frontend/build/* /var/www/html 
-
-# #delete the frontend
-# sudo rm -r /var/www/html/frontend
+#delete the frontend
+sudo rm -r /var/www/html/frontend
 
