34c53958d0ed72eda70f4503b601fe6644850880
Bopchy <rorbochere@gmail.com>
2022-03-21 18:21:32 +0300

    Copy build files to default web folder.

--- frontend_setup.sh
+++ frontend_setup.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 sudo apt update
 sudo apt install node.js nginx -y
+
+# Copy build files 
+sudo cp -r build/* /var/www/html
+# Remove nginx index file
+sudo rm -rf /var/www/html/index.nginx.debian.html
+
+# Restart nginx 
+sudo systemctl restart nginx
